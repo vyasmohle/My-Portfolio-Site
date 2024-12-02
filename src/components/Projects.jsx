f058285140278023160de84def7730db4fe94bb6
vyasMohle <vyasmohle1811@gmail.com>
2024-12-03 03:04:41 +0530

    Fixed duplicate key issue and updated deploy script

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ function Projects() {
     {
       width: "50vw",
       // height: "60vh",
-      bg: "bg-zinc-600",
+      
       name: "Movie Hub",
       bg: "url('https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       detail:
@@ -31,7 +31,7 @@ function Projects() {
     {
       width: "37vw",
       // height: "60vh",
-      bg: "bg-zinc-600",
+      
       name: "Obeys Agency (Animation UI) ",
       p: "3vh",
       bg: "url('https://images.unsplash.com/photo-1631879742077-0b48672bf910?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
@@ -52,7 +52,7 @@ function Projects() {
     {
       width: "35vw",
       // height: "60vh",
-      bg: "bg-zinc-600",
+      
       name: "Dribbble UI ",
       bg: "url('https://images.unsplash.com/photo-1696041760371-19e546d796f3?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       detail:
@@ -72,7 +72,7 @@ function Projects() {
     {
       width: "50vw",
       // height: "60vh",
-      bg: "bg-zinc-600",
+     
       name: "Mentor Connect ",
       bg: "url('https://images.unsplash.com/photo-1639322537231-2f206e06af84?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       detail:
