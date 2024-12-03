e3a18d256a9b8651eb6b84508f6d71ff948b984a
vyasMohle <vyasmohle1811@gmail.com>
2024-12-03 16:36:03 +0530

    basename change

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='My-Portfolio-Site'>
     <App />
     </BrowserRouter>
   </StrictMode>,
